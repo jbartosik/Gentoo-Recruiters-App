54c11534f4b6f8eaa86f8bfb49c8c06bdae877cf
Joachim Filip Ignacy Bartosik <jbartosik@gmail.com>
2010-05-25 18:54:28 +0200

    Removed answers index
    It had only broke counter and was useless.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,7 +2,7 @@ class AnswersController < ApplicationController
 
   hobo_model_controller
 
-  auto_actions :all
+  auto_actions :all, :except => :index
   index_action :my_recruits, :my_recruits_cat
 
 end
