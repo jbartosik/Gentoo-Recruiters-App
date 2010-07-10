c3f3624fa9a33e73f4dd580ed01dcb316c589ebf
Joachim Filip Ignacy Bartosik <jbartosik@gmail.com>
2010-07-10 22:43:17 +0200

    Receive email, update taglibs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
 
   auto_actions :all, :except => [ :index, :new, :create ]
   index_action :ready_recruits
-  index_action :mentorless_recruits
+  index_action :mentorless_recruits, :receive_email
+
 
   def ready_recruits
     hobo_index User.recruits_answered_all
@@ -12,5 +13,16 @@ class UsersController < ApplicationController
 
   def mentorless_recruits
     hobo_index  User.mentorless_recruits
+  end
+  def receive_email
+    hobo_index
+
+    mail          = TMail::Mail.new
+    mail.subject  = params['subject']
+    mail.from     = params['from']
+    mail.to       = params['to']
+    mail.reply_to = params['reply_to']
+
+    UserMailer.receive(mail.to_s)
   end
 end
