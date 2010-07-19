dca36097c60520d5a5bb59a3033f9a728a21b5bb
Joachim Filip Ignacy Bartosik <jbartosik@gmail.com>
2010-07-19 13:39:01 +0200

    Rename method in User model
    answered_all_multi_choice -> answered_all_multi_choice_questions?

--- app/models/user.rb
+++ app/models/user.rb
@@ -128,7 +128,7 @@ class User < ActiveRecord::Base
     end
   end
 
-  def answered_all_multi_choice
+  def answered_all_multi_choice_questions?
     Question.multiple_choice.ungrouped_questions_of_user(id).unanswered(id).count == 0 &&
       Question.multiple_choice.grouped_questions_of_user(id).unanswered(id).count == 0
   end
