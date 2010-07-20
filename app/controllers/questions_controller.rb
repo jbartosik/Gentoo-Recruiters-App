67035de90c2d1a48c154eb1728a5913aa60bf127
Joachim Filip Ignacy Bartosik <jbartosik@gmail.com>
2010-07-20 13:37:33 +0200

    Clean QuestionsController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,11 +6,11 @@ class QuestionsController < ApplicationController
   index_action :answered_questions, :unanswered_questions, :suggest_questions, :approve_questions
 
   def answered_questions
-    hobo_index (current_user.signed_up? && current_user.answered_questions)
+    hobo_index current_user.try.answered_questions
   end
 
   def suggest_questions
-    hobo_index Question.suggested_questions current_user.try.id
+    hobo_index Question.suggested_questions(current_user.try.id)
   end
 
   def approve_questions
