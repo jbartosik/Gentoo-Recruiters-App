5e20277be96d2900e3001607b80bb572defcac04
Joachim Filip Ignacy Bartosik <jbartosik@gmail.com>
2010-08-11 22:32:19 +0200

    Bugfix: spec for email answer was named improperly (and one test broke)

--- spec/models/email_answer_spec.rb
+++ spec/models/email_answer_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper.rb'
 describe EmailAnswer do
   it "should send email notification when parses answer for unrecognized question from known user" do
     recruit       = Factory(:recruit)
+    question      = Factory(:question)
     mail          = TMail::Mail.new
-    mail.subject  = "some subject"
+    mail.subject  = "#{question.id + 1}-#{recruit.token}"
     mail.from     = recruit.email_address
 
     UserMailer.should_receive(:deliver_unrecognized_email).with(recruit, mail)
