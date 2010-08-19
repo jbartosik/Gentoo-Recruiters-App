ddd715b3245808d726b3b6b1133b2eace6a96315
Joachim Filip Ignacy Bartosik <jbartosik@gmail.com>
2010-08-19 19:59:17 +0200

    Add copyright notice to *.rb files under lib/ and app/

--- app/models/question_group.rb
+++ app/models/question_group.rb
@@ -1,3 +1,17 @@
+#    Gentoo Recruiters Web App - to help Gentoo recruiters do their job better
+#   Copyright (C) 2010 Joachim Filip Bartosik
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, version 3 of the License
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # Users should answer only one question from each group. The question recruit
 # should answer is randomly chosen from group by application. Unless user is
 # recruit [s]he can't view grouped questions [s]he is not supposed to answer.
