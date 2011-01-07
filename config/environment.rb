1db84090103ca878d2d8977ceb6dda1f69afd1b2
Joachim Filip Ignacy Bartosik <jbartosik@gmail.com>
2011-01-07 17:37:18 +0100

    Fix cucumber tests
    Update to Rails 2.3.8 broke cucumber tests. This is fix from agibralter
    from https://gist.github.com/431811

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,22 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
 
+class RackRailsCookieHeaderHack
+  def initialize(app)
+    @app = app
+  end
+  def call(env)
+    status, headers, body = @app.call(env)
+    if headers['Set-Cookie'] && headers['Set-Cookie'].respond_to?(:collect!)
+      headers['Set-Cookie'].collect! { |h| h.strip }
+    end
+    [status, headers, body]
+  end
+end
+
 Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
+  config.after_initialize do
+    ActionController::Dispatcher.middleware.insert_before(ActionController::Base.session_store, RackRailsCookieHeaderHack)
+  end
 end
